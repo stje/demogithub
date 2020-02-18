b01d9592af80c1a7b02d48b45492b16cb7ea6998
stje <stje@easj.dk>
2020-02-18 15:50:39 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ProductService } from './product.service';
+
+@Component({
+  selector: 'app-root',
+  template:
+    `<h1>{{title}}</h1>
+   <products></products>`,
+  providers: [ProductService]
+})
+export class AppComponent {
+  title = 'My second Angular app !!!!!';
+}
